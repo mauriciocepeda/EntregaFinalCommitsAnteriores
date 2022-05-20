d9276f0985e7aaa0d75ab84e42f0e7c1937a3e88
Mauricio Cepeda <mauricepeda@hotmail.com>
2022-05-20 13:46:10 -0300

    commit del dia 20 de mayo de 2022, se crea vista de login y register, falta quitarle los comentarios al register creando nuestro form del mismo. tambien falta crear el logout y los mixin y decoradores, como a su vez organizar la web para que solo se pueda acceder estando registrado

--- proyectofinal/appmodel/urls.py
+++ proyectofinal/appmodel/urls.py
@@ -1,15 +1,21 @@
 from django import views
 from proyectofinal.urls import path
-from .views import ReseñaLista,ReseñaDetalle, ReseñaCrear, ReseñaEditar,ReseñaBorrar, inicio
+from appmodel.views import inicio, ReseñaLista, ReseñaDetalle, ReseñaCrear, ReseñaEditar, ReseñaBorrar
+from appmodel.views import login_request, register
 
 urlpatterns = [
-    path('', inicio, name='inicio'),
+    path('inicio', inicio, name='inicio'),
 
-
+#---------------reseñas crud --------------------------
     path('reseñas', ReseñaLista.as_view(), name='reseñas'),
     path('reseñas/<pk>', ReseñaDetalle.as_view(), name='reseñadetalle'),
     path('reseñas/crear/', ReseñaCrear.as_view(), name='reseñacrear'),
     path('reseñas/editar/<pk>', ReseñaEditar.as_view(), name='reseñaeditar'),
     path('reseñas/borrar/<pk>', ReseñaBorrar.as_view(), name='reseñaborrar'),
 
+#---------------Login-Logout-Registro------------------------------------------
+
+    path('login_request', login_request , name ='login'),
+    path('register', register , name ='register'),
+    
 ]
--- proyectofinal/appmodel/views.py
+++ proyectofinal/appmodel/views.py
@@ -1,8 +1,13 @@
+
+from tkinter import E
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from django.http import HttpResponse, request
 from .models import Reseña
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+
 
 def inicio(request):
     return render (request, 'appmodel/inicio.html')
@@ -22,18 +27,56 @@ class ReseñaDetalle(DetailView):
 
 class ReseñaCrear(CreateView):
     model=Reseña
-    success_url=reverse_lazy('listareseñas')
+    success_url=reverse_lazy('reseñas')
     fields=['titulo','fecha','cuerpo', 'valoracion']
 
 class ReseñaEditar(UpdateView):
     model=Reseña
-    success_url=reverse_lazy('listareseñas')
+    success_url=reverse_lazy('reseñas')
     fields=['titulo','fecha','cuerpo','valoracion']
 
 class ReseñaBorrar(DeleteView):
     model=Reseña
-    sucess_url=reverse_lazy('listareseñas')
+    success_url=reverse_lazy('reseñas')
     fields=['titulo','fecha','cuerpo','valoracion']
 
 
-#-----------------------------------------
+#---------------------Login, Logout, Registrate---------
+
+def login_request(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            nombre=form.cleaned_data.get('username')
+            contraseña=form.cleaned_data.get('password')
+            user=authenticate(username=nombre, password=contraseña)
+
+            if user is not None:
+                login(request, user)
+
+                return render(request, 'appmodel/reseñas.html')
+            else:
+                return render(request, 'appmodel/inicio.html')
+        else:
+            return render(request, 'appmodel/inicio.html')
+    else:
+        form=AuthenticationForm()
+        return render(request, 'appmodel/login.html', {"form":form} )
+
+def register(request):
+    if request.method=="POST":
+        form=UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user=form.cleaned_data.get('username')
+            form.save()
+            return render(request, 'appmodel/inicio.html', {"mensaje": "usuario creado"})
+        else:
+            return render(request, 'appmodel/login.html',{"mensaje":"algo falle, el usuario no pudo crearse"})
+
+    else:
+        form=UserCreationForm()
+        return render(request, 'appmodel/login.html',{"form":form})
+
+
+
